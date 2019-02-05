bc5b3b8c4c4abd7119025a0bdd0bd02fa0f85d3b
simmoe <simmoe@gmail.com>
2019-02-05 16:07:28 +0100

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+
+    //Først en variabel til din api-key
+    var apiKey = "g6R76FJJwgaLM8lQCROIJBHQY8BH3Brx";    
+    //Så en hemmelighed, som vi måske skal bruge
+    var secret = "QAZUx85Ga9lNiinY";
+    //Og nu til den base url der giver adgang til api'et 
+    var url = "http://api.nytimes.com/svc/books/v3/reviews.json";
+    //Dernæst bygger du den query du vil udføre
+    var query = "?author=Jonathan+Safran+Foer";
+        query += "&noget=nogetandet";
+    
+    
+    
+    createElement("h1", "Spørg New York Times API");
+    
+    
+    
+    loadJSON(url + query + "&api-key=" + apiKey, gotData);
+    
+    function gotData(data) {
+        console.log(data);
+        createElement("h5", data.copyright);
+        data.results.forEach(function(e){
+            createElement("h5", e.book_title);
+        });
+    }
+}
